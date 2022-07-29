0e5277ee24741b1184c4a21d3a991120dd344cae
CiprianOlteanu23 <ciprian.olteanu23@gmail.com>
2022-07-29 10:37:59 +0300

    added SwapItems method
    the method has +3 overloads for the posible parameter combinations

--- Generics/GenericCollection.cs
+++ Generics/GenericCollection.cs
@@ -22,5 +22,23 @@ namespace Generics
             get => _values[index];
             set => _values[index] = value;
         }
+        public void SwapItems(int index1, int index2)
+        {
+            if (index1 == index2)
+                throw new Exception("Indexes must be different!");
+            (_values[index1], _values[index2]) = (_values[index2],_values[index1]);
+        }
+        public void SwapItems( T value1, T value2)
+        {
+            SwapItems(Array.IndexOf(_values, value1), Array.IndexOf(_values, value2));
+        }
+        public void SwapItems(int index1, T value2)
+        {
+            SwapItems(index1, Array.IndexOf(_values, value2));
+        }
+        public void SwapItems(T value1, int index2)
+        {
+            SwapItems(Array.IndexOf(_values,value1),index2);
+        }
     }
 }
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,5 +1,13 @@
 ﻿using Generics;
 
-var collection = new GenericCollection<int>(10);
-collection[0] = 1;
-Console.WriteLine(collection[0]);
+var collection = new GenericCollection<string>(10);
+collection[0] = "1";
+collection[1] = "2";
+try
+{
+    collection.SwapItems(0, collection[0]);
+} 
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
